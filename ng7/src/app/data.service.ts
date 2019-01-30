502dd98c4b3e46346ad241d76404b67f8c1af0db
rkw <bartoszbrzozowskipl@gmail.com>
2019-01-30 19:52:08 +0100

    #3

--- ng7/src/app/data.service.ts
+++ ng7/src/app/data.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
-  firstClick()
+  constructor(private http:HttpClient) { }
+  getUsers()
   {
-    return console.log('clicked');
+    return this.http.get('https://reqres.in/api/users');
   }
 
 }
--- ng7/src/app/home/home.component.ts
+++ ng7/src/app/home/home.component.ts
@@ -9,14 +9,20 @@ import {DataService} from '../data.service';
 export class HomeComponent implements OnInit {
 
   h1Style:boolean = false;
+
+  users: Object;
   constructor(private _dataService:DataService) { }
 
   ngOnInit() {
+    this._dataService.getUsers().subscribe(_dataService => { this.users = _dataService
+    console.log(this.users)
+    } );
+    
   }
 
   firstClick()
   {    
-    this.h1Style = true;
+    this.h1Style = true;    
   }
 
 }
